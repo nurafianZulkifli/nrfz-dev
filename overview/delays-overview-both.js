b60a6cdd7d0ecf2ec30fae848f5c9fb2ff703293
nrfz <nurafian.bz33@gmail.com>
2024-12-30 00:55:34 +0800

    2025-XX Feature Update for Version Next

---
+++ overview/delays-overview-both.js
@@ -0,0 +1,44 @@
+
+const doverview24sm = document.getElementById('delays-overview-sm-24').getContext('2d');
+
+const doverviewsm24 = new Chart(doverview24sm, {
+  type: 'line',
+  data: {
+    labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    datasets: [{
+      label: "SMRT Trains",
+      data: [1,1,2,1,1,1,2,2,9,3,0,6],
+      borderColor: 'rgb(207, 61, 61)',
+      fill: true,
+      backgroundColor: 'rgba(207, 61, 61,0.05)',
+      borderWidth: 3
+    },
+
+    {
+      label: "SBS Transit",
+      data: [2,0,0,1,2,1,0,0,0,0,0,0],
+      borderColor: 'rgb(130, 71, 148)',
+      fill: true,
+      backgroundColor: 'rgba(130, 71, 148,0.05)',
+      borderWidth: 3
+    }]//
+  },
+
+
+
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    },
+    plugins: {
+      legend: {
+        display: true
+      },
+    }
+  }
+});
