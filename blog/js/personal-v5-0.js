e84d94ced1bf206f139b578415acc02f733b208a
nurafianZulkifli <menacingaskanois@gmail.com>
2025-04-20 13:46:53 +0800

    2025-06 Feature Update for Version Next (5.0.x)

---
+++ blog/js/personal-v5-0.js
@@ -0,0 +1,38 @@
+/* Dark Mode Functionality for Individual Pages */
+
+// Check localStorage for dark mode preference
+if (localStorage.getItem('dark-mode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+    updateHrefForDarkMode();
+}
+
+const toggleButton = document.getElementById('dark-mode-toggle');
+toggleButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    // Save the preference in localStorage
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('dark-mode', 'enabled');
+    } else {
+        localStorage.setItem('dark-mode', 'disabled');
+    }
+    updateHrefForDarkMode();
+});
+
+function updateHrefForDarkMode() {
+    /* Banners */
+    const pv5Page = document.getElementById('cv-pv5');
+
+    const isDarkMode = document.body.classList.contains('dark-mode');
+
+    if (isDarkMode) {
+        /* Banners */
+        pv5Page.style.backgroundImage = "url(https://i.imgur.com/bOE19OL.png)";
+
+
+    } else {
+        /* Banners */
+        pv5Page.style.backgroundImage = "url(https://i.imgur.com/JfP4t5u.png)";
+    }
+
+
+}
