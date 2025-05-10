27a4a53ffffaf3f097c27203c08467ce555d458f
nurafianZulkifli <menacingaskanois@gmail.com>
2025-05-10 12:55:43 +0800

    2025-05 Feature Update for Version 2505 (5.0.9a)

--- eicw/js/eicw-2.js
+++ eicw/js/eicw-2.js
@@ -3,7 +3,10 @@
 // Check localStorage for dark mode preference
 if (localStorage.getItem('dark-mode') === 'enabled') {
     document.body.classList.add('dark-mode');
+    updateThemeIcon('dark');
     updateHrefForDarkMode();
+} else {
+    updateThemeIcon('light');
 }
 
 const toggleButton = document.getElementById('dark-mode-toggle');
@@ -12,11 +15,25 @@ toggleButton.addEventListener('click', () => {
     // Save the preference in localStorage
     if (document.body.classList.contains('dark-mode')) {
         localStorage.setItem('dark-mode', 'enabled');
+        updateThemeIcon('dark');
     } else {
         localStorage.setItem('dark-mode', 'disabled');
+        updateThemeIcon('light');
     }
     updateHrefForDarkMode();
 });
+
+// Function to update the theme icon
+function updateThemeIcon(theme) {
+    const themeIcon = document.getElementById('theme-icon');
+    if (theme === 'dark') {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    } else {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    }
+}
 
 function updateHrefForDarkMode() {
     /* Banners */
