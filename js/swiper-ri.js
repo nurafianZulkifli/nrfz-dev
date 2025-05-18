5aed6725dee595f89ab9d5c8baf8bca6a93e64fd
nurafianZulkifli <menacingaskanois@gmail.com>
2025-05-18 10:36:09 +0800

    2025-06 Feature Update for Version Next (5.0.x)

--- js/swiper-ri.js
+++ js/swiper-ri.js
@@ -2,27 +2,28 @@ var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 25,
     loop: false,
-    centerSlide: 'true',
-    fade: 'true',
-    grabCursor: 'true',
+    grabCursor: true,
     pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
+        el: ".swiper-pagination",
+        clickable: true,
+        dynamicBullets: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
     },
-    breakpoints:{
+    breakpoints: {
         0: {
             slidesPerView: 1,
         },
-        520: {
+        576: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1272: {
             slidesPerView: 3,
-        },
-        950: {
-            slidesPerView: 4,
-        },
-    },
-  });
+        }
+    }
+});
