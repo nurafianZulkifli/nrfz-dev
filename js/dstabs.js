b56460a21c5a8723308039f92aa54bb7659ae1bd
_nrfz <63917225+nurafianZulkifli@users.noreply.github.com>
2025-09-25 23:26:04 +0800

    XXXX-XX Feature Update for Version Next (6.0.0)

--- js/dstabs.js
+++ js/dstabs.js
@@ -1,58 +1,26 @@
-//script for tabs
-jQuery().ready(function () {
-    initTabs($(".tabs"));
-});
-
-function initTabs(tabs) {
-    shiftSelector(tabs);
-
-    $(window).resize(function () {
-        shiftSelector(tabs);
-    });
-
-    tabs.on("click", "a", function (e) {
-        e.preventDefault();
-        tabs.find("a").removeClass("active");
-        $(this).addClass("active");
-
-        shiftSelector(tabs);
-    });
-}
-
-function shiftSelector(tabs) {
-    let activeItem = tabs.find(".active");
-    let activeWidth = activeItem.innerWidth();
-    let activeHeight = activeItem.innerHeight();
-    let itemPos = activeItem.position();
-    tabs.find(".selector").css({
-        left: itemPos.left + "px",
-        width: activeWidth + "px",
-        height: activeHeight + "px",
-        top: itemPos.top + "px"
-    });
-
-    let activeBody = $(".tabs .active").attr("data-body");
-    $(".tab-body > section").hide();
-    $("." + activeBody).show(); // Replaced fadeIn() with show() to remove animation
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('nav.tabs a');
-
-    tabs.forEach(tab => {
-        tab.addEventListener('click', (event) => {
-            event.preventDefault(); // Prevent default anchor behavior
-            const href = tab.getAttribute('href');
-            const dataBody = tab.getAttribute('data-body');
-
-            // Redirect to the href or data-body if available
-            if (dataBody) {
-                window.location.href = dataBody;
-            } else if (href) {
-                window.location.href = href;
-            }
-        });
+// ----------swiper-slider---------
+var swiper = '';
+$(window).on('load', function(){
+    swiper = new Swiper('#catgory-slider', {
+        loop: false,
+        slidesPerView: "auto",
+        allowTouchMove: this,
+        spaceBetween: 5,
+        mousewheel: true,
+        slideToClickedSlide: true,
+        centeredSlides: false,
+        navigation: {
+            nextEl: '.slider-next',
+            prevEl: '.slider-prev',
+        }
     });
 });
 
+
+$(".category-button").click(function(){
+    $(".category-button").removeClass("active");
+    $(this).addClass('active');
+    var getid = $(this).data('id');
+    $(".data-text").removeClass('active');
+    $("#"+getid).addClass("active");
+});
