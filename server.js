4221b3826e137c0e21ffb06b7ac28ad3477acebf
nrfz <menacingaskanois@gmail.com>
2025-11-22 12:08:20 +0800

    Fix server initialization

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const LTA_API_KEY = process.env.LTA_API_KEY; // Store your API key in environmen
 // Define the route
 app.get('/bus-arrivals', async (req, res) => {
   try {
-    const response = await axios.get('http://datamall2.mytransport.sg/ltaodataservice/BusArrivalv2', {
+    const response = await axios.get('https://datamall2.mytransport.sg/ltaodataservice/v3/BusArrival', {
       headers: {
         AccountKey: LTA_API_KEY,
         accept: 'application/json',
