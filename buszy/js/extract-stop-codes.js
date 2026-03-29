49c7431b3301b462a30c60c12492f98f9566c422
nrfz <63917225+nurafianZulkifli@users.noreply.github.com>
2026-03-29 21:11:29 +0800

    2026-05 Feature Update for NRFZ

--- buszy/js/extract-stop-codes.js
+++ buszy/js/extract-stop-codes.js
@@ -3,7 +3,17 @@ const filePath = 'd:\\GitHub\\nrfz-dev\\buszy\\json\\StopCode.txt';
 const jsonPath = 'd:\\GitHub\\nrfz-dev\\buszy\\json\\stop-codes.json';
 
 const content = fs.readFileSync(filePath, 'utf8');
-const codes = content.match(/\b\d{5}\b/g);
+const allCodes = content.match(/\b\d{5}\b/g);
+const seen = new Set();
+const codes = [];
+
+// Iterate in reverse to get last occurrence of each code
+for (let i = allCodes.length - 1; i >= 0; i--) {
+  if (!seen.has(allCodes[i])) {
+    codes.unshift(allCodes[i]);
+    seen.add(allCodes[i]);
+  }
+}
 
 // Save as JSON
 const jsonData = { st: codes };
